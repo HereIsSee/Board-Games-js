6392de702bd57a09f8bdfedb4576c3da8b777c56
unknown <biramantvydas@gmail.com>
2024-03-21 15:04:27 +0200

    This is the react project will see if this works or not

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Board from './components/Board/Board'
+
+function App() {
+  <div className='peptalk'> hello</div>
+  return (
+    <div className="App">
+      <Board/>
+    </div>
+  );
+  
+}
+
+export default App;
