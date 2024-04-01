933c38888de0ac6ba42e075af93ffed73cfe9ca9
unknown <biramantvydas@gmail.com>
2024-04-01 20:14:17 +0300

    Added the initial interface and a button to go back from the board game

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import { useReducer } from 'react';
+import React, { useState, useReducer } from 'react';
 import './App.css';
-import Board from './components/Board/Board'
+import './styles/button.css';
+import './styles/dark.css';
 import AppContext from './contexts/Context';
-import { reducer } from './reducer/reducer'
+import { reducer } from './reducer/reducer';
 import { initGameState } from './constant';
+import Board from './components/Board/Board';
 
 function App() {
+  const [appState, dispatch] = useReducer(reducer, initGameState);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedGame, setSelectedGame] = useState(null);
 
-  const [appState, dispatch] = useReducer(reducer, initGameState)
+  const handlePlayClick = () => {
+    setShowModal(true);
+  };
 
-  const providerState = {
-    appState,
-    dispatch
-  }
+  const handleGameSelect = (game) => {
+    setSelectedGame(game);
+    setShowModal(false);
+    setIsPlaying(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  const handleGoBack = () => {
+    setIsPlaying(false);
+    setSelectedGame(null);
+  };
+
   return (
-    <AppContext.Provider value={providerState} >
-        <div className="App">
-            <Board/>
-        </div>
+    <AppContext.Provider value={{ appState, dispatch }}>
+      <div className="App">
+        {isPlaying ? (
+          <Board onGoBack={handleGoBack} />
+        ) : (
+          <button className="landing-button" id="play-button" onClick={handlePlayClick}>Play</button>
+        )}
+        {showModal && (
+          <div className="modal">
+            <div className="modal-content">
+              <span className="close" onClick={handleCloseModal}>&times;</span>
+              <h2>Choose a Game</h2>
+              <button className="game-option" onClick={() => handleGameSelect('Chess')}>Chess</button>
+              <button className="game-option" onClick={() => handleGameSelect('Checkers')}>Checkers</button>
+              <button className="game-option" onClick={() => handleGameSelect('Random')}>Random</button>
+            </div>
+          </div>
+        )}
+      </div>
     </AppContext.Provider>
-); 
-    
+  );
 }
 
 export default App;
--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,51 +1,49 @@
-import './Board.css'
+import React from 'react';
+import './Board.css';
+import '../../styles/button.css'
+import Ranks from './bits/Ranks';
+import Files from './bits/Files';
+import Pieces from '../Pieces/Pieces';
+import { useAppContext } from '../../contexts/Context';
 
-import Ranks from './bits/Ranks'
-import Files from './bits/Files'
-import Pieces from '../Pieces/Pieces'
-import { useAppContext } from '../../contexts/Context'
+const Board = ({ onGoBack }) => {
+  const ranks = Array(8).fill().map((x, i) => 8 - i);
+  const files = Array(8).fill().map((x, i) => i + 1);
+  const { appState } = useAppContext();
+  const position = appState.position[appState.position.length - 1];
 
-const Board = () => {
-    const ranks = Array(8).fill().map((x,i) => 8-i)
-    const files = Array(8).fill().map((x,i) => i+1)
+  const getClassName = (i, j) => {
+    let c = 'tile';
+    c += (i + j) % 2 === 0 ? ' tile--dark ' : ' tile--light ';
 
-    const {appState} = useAppContext()
-    const position = appState.position[appState.position.length-1]
-
-    const getClassName = (i,j) => {
-        let c = 'tile'
-        c+= (i+j)%2 === 0 ? ' tile--dark ' : ' tile--light '
-
-        if (appState.candidateMoves?.find(m => m[0] === i && m[1] === j)) {
-            if (position[i][j])
-                c+= ' attacking'
-            else
-                c+= ' highlight'
-        }
-        
-        return c
+    if (appState.candidateMoves?.find(m => m[0] === i && m[1] === j)) {
+      if (position[i][j]) c += ' attacking';
+      else c += ' highlight';
     }
 
-    return <div className='board'>
+    return c;
+  };
 
-        <Ranks ranks={ranks}/>
+  return (
+    <div>
+      <button id="play-button" onClick={onGoBack}>Go Back</button>
+      <div className="board">
+        <Ranks ranks={ranks} />
 
-        <div className='tiles'>
-            {ranks.map((rank,i) => 
-                files.map((file,j) => 
-                    <div 
-                        key={file+''+rank} 
-                        className={getClassName(7-i,j)}>
-                    </div>
-                ))}
+        <div className="tiles">
+          {ranks.map((rank, i) =>
+            files.map((file, j) => (
+              <div key={file + '' + rank} className={getClassName(7 - i, j)}></div>
+            ))
+          )}
         </div>
 
-        <Pieces/>
+        <Pieces />
 
-        <Files files={files}/>
-
+        <Files files={files} />
+      </div>
     </div>
-    
-}
+  );
+};
 
-export default Board
+export default Board;
