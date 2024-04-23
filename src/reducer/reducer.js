2f35b7d1276cad56fe1b952144f5da86ef72c5ee
Greta Pavilionė <greta.pavilione@gmail.com>
2024-04-23 17:52:45 +0300

    Make actions when insufficient material

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -57,7 +57,25 @@ export const reducer = (state, action) => {
       };
     }
 
-    
+    case actionTypes.STALEMATE: {
+      return {
+        ...state,
+        status : Status.stalemate,
+      };
+    }
+
+    case actionTypes.INSUFFICIENT_MATERIAL: {
+      return {
+        ...state,
+        status : Status.insufficient,
+      };
+    }
+
+    case actionTypes.NEW_GAME: {
+      return {
+        ...action.payload
+      };
+    }
 
     default:
       return state;
